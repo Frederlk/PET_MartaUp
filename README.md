e7e6d30a0e1e2bca8c67cca9155a5aff785c2e41
Sergey Petrinich <petrinichshadow@gmail.com>
2022-10-18 21:26:11 +0300

    firebase deploy

---
+++ README.md
@@ -0,0 +1,4 @@
+## Links
+
+<a href="https://pet-marta.web.app">Home Page [1 link]</a> \
+<a href="https://pet-marta.firebaseapp.com">Home Page [2 link]</a>
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,29 +1,10 @@
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
 import Swiper, { EffectFade, Pagination, Autoplay } from "swiper";
-/*
-Основниые модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
 
-// Стили Swiper
-// Базовые стили
 import "../../scss/base/swiper.scss";
 
-// Инициализация слайдеров
 function initSliders() {
-    // Перечень слайдеров
-    // Проверяем, есть ли слайдер на стронице
     if (document.querySelector(".home__slider")) {
-        // Указываем скласс нужного слайдера
-        // Создаем слайдер
         new Swiper(".home__slider", {
-            // Указываем скласс нужного слайдера
-            // Подключаем модули слайдера
-            // для конкретного случая
             modules: [Pagination, EffectFade, Autoplay],
             observer: true,
             observeParents: true,
@@ -32,27 +13,19 @@ function initSliders() {
             autoHeight: true,
             speed: 500,
             effect: "fade",
-            // Дополнение к fade
             fadeEffect: {
-                // Параллельная
-                // смена прозрачности
                 crossFade: true,
             },
             pagination: {
                 el: ".home__pagination",
-
-                // Буллеты
                 type: "bullets",
                 clickable: true,
             },
             watchOverflow: true,
             autoHeight: false,
             autoplay: {
-                // Пауза между прокруткой
                 delay: 4000,
-                // Закончить на последнем слайде
                 stopOnLastSlide: true,
-                // Отключить после ручного переключения
                 disableOnInteraction: false,
             },
             loop: true,
@@ -60,8 +33,5 @@ function initSliders() {
     }
 }
 window.addEventListener("load", function (e) {
-    // Запуск инициализации слайдеров
     initSliders();
-    // Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
-    //initSlidersScroll();
 });
